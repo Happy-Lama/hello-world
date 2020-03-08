4c833a64b9d2c0b17662ab4b383fdc745f148329
Happy-Lama <kldenis2001@gmail.com>
2020-03-09 00:34:39 +0300

    Calc

---
+++ CalculatorProject2.py
@@ -0,0 +1,47 @@
+while True:
+    Operations = "Add Subtract Multiply Divide"
+    print(Operations)
+    user_input = input()
+    if user_input == "Add" or user_input == "add":
+        print("Input the two values to be added(separate with a space):")
+        user_input = input()
+        numbers = user_input.split()
+        y = 0
+        for i in numbers:
+          numbers[y]  = int(i)
+          y +=  1
+        print(numbers[0] + numbers[1])
+        continue
+    elif user_input == "Subtract" or user_input == "subtract":
+        print("Input the two values to be subtracted(separate with a space):")
+        user_input = input()
+        numbers = user_input.split()
+        y = 0
+        for i in numbers:
+          numbers[y]  = int(i)
+          y +=  1
+        print(numbers[0] - numbers[1])
+        continue
+    elif user_input == "Multiply" or user_input == "multiply":
+        print("Input the two values to be multiplied(separate with a space):")
+        user_input = input()
+        numbers = user_input.split()
+        y = 0
+        for i in numbers:
+          numbers[y]  = int(i)
+          y +=  1
+        print(numbers[0] * numbers[1])
+        continue
+    elif user_input == "Divide" or user_input == "divide":
+        print("Input the two values to be divided(separate with a space and divisor last):")
+        user_input = input()
+        numbers = user_input.split()
+        y = 0
+        for i in numbers:
+          numbers[y]  = int(i)
+          y +=  1
+        print(numbers[0] / numbers[1])
+        continue
+    else:
+        print("Cant perform operation!!")
+        continue
